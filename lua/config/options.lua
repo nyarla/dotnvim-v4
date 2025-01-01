6ab9aadb025dd068891ae822c3fcb8115088a069
nyarla <nyarla@kalaclista.com>
2025-01-01 12:35:25 +0900

    chore(kalaclista.options): import options from dotnvim-v3

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,9 +1,52 @@
+local lib = require("lib.options")
+local opt = lib.opt
+
 local M = {}
 
 function M.setup()
-  vim.opt.cursorline = true
-  vim.opt.number = true
-  vim.opt.termguicolors = true
+  -- make global options
+  local options = {
+    -- encoding
+    encoding = "utf-8",
+    fileencoding = "utf-8",
+    -- ui
+    ambiwidth = "single",
+    cursorline = true,
+    number = true,
+    scl = "yes",
+    termguicolors = true,
+    showtabline = 2,
+    -- cache and backup
+    backupdir = vim.fn.stdpath("cache") .. "/backup",
+    directory = vim.fn.stdpath("cache") .. "/swap",
+    -- edit
+    clipboard = "unnamed,unnamedplus",
+    mouse = "a",
+    -- editor
+    autoindent = true,
+    backspace = "indent,eol,start",
+    expandtab = true,
+    shiftwidth = 2,
+    softtabstop = 2,
+    tabstop = 4,
+    updatetime = 500,
+  }
+
+  -- set global options
+  for key, value in pairs(options) do
+    opt(key, value)
+  end
+
+  -- set wcwidth
+  vim.fn.setcellwidths(require("lib.wcwidth"))
+
+  -- set diagnostic config
+  vim.diagnostic.config({
+    float = {
+      scope = "line",
+      border = "rounded",
+    },
+  })
 end
 
 return M
