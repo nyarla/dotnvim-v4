fbe4ba57a1b88f1ca2bf995cbacd76cdc40b6415
nyarla <nyarla@kalaclista.com>
2025-01-02 15:37:44 +0900

    feat(plugins.nvim-cmp): enable to use auto completion by nvim-cmp

---
+++ lua/plugins/cmp-cmdline.lua
@@ -0,0 +1,9 @@
+local spec = {
+  -- source
+  "hrsh7th/cmp-cmdline",
+  commit = "d250c63aa13ead745e3a40f61fdd3470efde3923",
+  -- loading
+  lazy = true,
+}
+
+return spec
---
+++ lua/plugins/cmp-treesitter.lua
@@ -0,0 +1,13 @@
+local spec = {
+  -- source
+  "ray-x/cmp-treesitter",
+  commit = "958fcfa0d8ce46d215e19cc3992c542f576c4123",
+  -- config
+  dependencies = {
+    "nvim-treesitter",
+  },
+  -- loading
+  lazy = true,
+}
+
+return spec
