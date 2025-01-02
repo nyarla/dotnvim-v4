73d4da1feb7f84d8c7ddecb3180cff43e3dbba82
nyarla <nyarla@kalaclista.com>
2025-01-02 12:42:17 +0900

    feat(plugin.perl): add vim-perl plugin

---
+++ lua/plugins/perl.lua
@@ -0,0 +1,10 @@
+local spec = {
+  -- source
+  "vim-perl/vim-perl",
+  commit = "25ecb0061a3558d242a471b162aad20e4308815d",
+  build = "make clean carp heredoc-sql highlight-all-pragmas method-signatures moose test-more try-tiny object-pad",
+  -- loading
+  ft = { "perl" },
+}
+
+return spec
