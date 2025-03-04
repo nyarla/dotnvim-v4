47ed5a112ef308eafcc0a54a5938637c0a79ab1b
nyarla <nyarla@kalaclista.com>
2025-03-04 15:19:34 +0900

    chore(plugins.nvim-lspconfig): update to v1.7.0

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,7 +1,7 @@
 local spec = {
   -- source
   "neovim/nvim-lspconfig",
-  commit = "66bc018936c6ff76beb75f89d986af6442db4001",
+  commit = "6b63bdf2399b9bedf93297d98419550523a9ad68", -- v1.7.0
   -- loading
   lazy = true,
 }
